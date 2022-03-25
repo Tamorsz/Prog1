93826ea70f98b9b5cdc68c21e71fbb2e3de28009
Tamorsz <99185457+Tamorsz@users.noreply.github.com>
2022-03-25 21:45:09 +0100

    drill 16 gui update

--- week02-06/GUI/GUI.cpp
+++ week02-06/GUI/GUI.cpp
@@ -78,3 +78,70 @@ int Menu::attach(Button* p)
 {
 	return attach(*p);
 }
+
+Lines_window::Lines_window(Point xy, int w, int h, const string& title)
+		: Window{ xy, w, h, title.c_str() }
+		, m_colourMenu	{ Point{double(x_max() - 70), 40}, 70, 20, Menu::vertical, "colour" }
+		, m_menuButton	{ Point{double(x_max() - 80), 30}, 80, 20, "colour menu", cb_menu }
+		, m_lsMenu		{ Point{double(x_max() - 70), 100}, 70, 20, Menu::vertical, "line style" }
+		, m_lsButton	{ Point{double(x_max() - 100), 100}, 100, 20, "line style menu", cb_lsMenu }
+		, m_nextButton	{ Point{double(x_max() - 150), 0}, 70, 20, "Next Point",
+						[](Address, Address pw) {reference_to<Lines_window>(pw).next(); } }
+		, m_quitButton	{ Point{double(x_max() -  70), 0}, 70, 20, "Quit",
+						[](Address, Address pw) {reference_to<Lines_window>(pw).quit(); } }
+		, m_nextX		{ Point{double(x_max() - 310), 0}, 50, 20, "next x:" }
+		, m_nextY		{ Point{double(x_max() - 210), 0}, 50, 20, "next y:" }
+		, m_xyOut		{ Point{100,0}, 100, 20, "current (x,y):" }
+	{
+		attach(m_nextButton);
+		attach(m_quitButton);
+		attach(m_nextX);
+		attach(m_nextY);
+		attach(m_xyOut);
+		attach(m_lines);
+
+		m_colourMenu.attach(new Button{ Point(0,0), 0, 0, "red", cb_red });
+		m_colourMenu.attach(new Button{ Point(0,0), 0, 0, "blue", cb_blue });
+		m_colourMenu.attach(new Button{ Point(0,0), 0, 0, "black", cb_black });
+		attach(m_colourMenu);
+
+		m_colourMenu.hide();
+		attach(m_menuButton);
+
+		m_lsMenu.attach(new Button{ Point(0,0), 0, 0, "solid",
+			[](Address, Address addr) {reference_to<Lines_window>(addr).changeStyle(Line_style::solid); } 
+			});
+		m_lsMenu.attach(new Button{ Point(0,0), 0, 0, "dash",
+			[](Address, Address addr) {reference_to<Lines_window>(addr).changeStyle(Line_style::dash); }
+			});
+		m_lsMenu.attach(new Button{ Point(0,0), 0, 0, "dot",
+			[](Address, Address addr) {reference_to<Lines_window>(addr).changeStyle(Line_style::dot); }
+			});
+		m_lsMenu.attach(new Button{ Point(0,0), 0, 0, "dashdot",
+			[](Address, Address addr) {reference_to<Lines_window>(addr).changeStyle(Line_style::dashdot); }
+			});
+		m_lsMenu.attach(new Button{ Point(0,0), 0, 0, "dashdotdot",
+			[](Address, Address addr) {reference_to<Lines_window>(addr).changeStyle(Line_style::dashdotdot); }
+			});
+		attach(m_lsMenu);
+
+		m_lsMenu.hide();
+		attach(m_lsButton);
+	}
+
+void Lines_window::next()
+	{
+		double x = m_nextX.get_int();
+		double y = m_nextY.get_int();
+		m_lines.add(Point{ x,y });
+		ostringstream ss;
+		ss << '(' << x << ',' << y << ')';
+		m_xyOut.put(ss.str());
+
+		redraw();
+	}
+
+void Lines_window::quit()
+	{
+		hide();		
+	}
